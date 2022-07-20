d4d95af0a87a1c677bb67cca22dc8daa715a4dfa
liuzhimin <zack@163.com>
2022-07-20 11:29:05 +0800

    create

--- README.rst
+++ README.rst
@@ -1,3 +1,3 @@
 
 
-# This is my first package;
+This is my first package
--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from distutils.core import setup
 from setuptools import find_packages
 
@@ -8,19 +10,15 @@ setup(name='work_tools',  # 包名
     version='1.0.0',  # 版本号
     description='A small example package',
     long_description=long_description,
-    author='zack liu',
-    author_email='1056559106@qq.com',
-    install_requires=[
-
-    ],
+    author='zack_liu',
+    author_email='liu.zhimin.2019@gmail.com',
+    url='https://github.com/liuzhimin2019/work_tools.git',
     license='BSD License',
     packages=find_packages(),
     platforms=["all"],
     classifiers=[
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
-        'Natural Language :: Chinese (Simplified)',
         'Programming Language :: Python :: 3.7',
-        'Topic :: Software Development :: Libraries'
     ],
 )
